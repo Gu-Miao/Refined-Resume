a8c2e6d042f5bdd679b5459d8616f7718d9c7e84
duoleyisui <1694474987@qq.com>
2018-06-12 09:15:42 +0800

    repair some bugs

--- src/index/js/welcome.js
+++ src/index/js/welcome.js
@@ -5,13 +5,13 @@ var c_welcome = function() {
 		'<img src="images/logo_welcome.png" width="100%" height="100%">'+
 		'</div>'+
 		'<div class="row">'+
-		'<div class="col-lg-10 mx-auto">'+
+		'<div class="wl-title">'+
 		'<h1 class="text-uppercase">'+
 		'<strong>帮助您创建一份更加精致而又出色的简历！</strong>'+
 		'</h1>'+   
 		'<hr>'+
 		'</div>'+
-		'<div class="col-lg-8 mx-auto">'+
+		'<div class="wl-context">'+
 		'<p class="text-faded mb-5">全球已将超过200000000人使用Refinde_Resume轻松找到工作!</p>'+
 		'<a class="btn" href="#/login">&nbsp;&nbsp;&nbsp;&nbsp;立&nbsp;即&nbsp;体&nbsp;验&nbsp;&nbsp;&nbsp;&nbsp;</a>'+
 		'</div>'+
